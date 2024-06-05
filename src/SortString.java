7ebe662beaea36ae58c8189372a63c00a2941fd3
jkhan94 <hjkwork@naver.com>
2024-06-05 12:08:04 +0900

    Add : 문자열 내 마음대로 정렬하기

---
+++ src/SortString.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class SortString {
+    public static void main(String[] args) {
+        String[] strings = {"abce", "abcd", "cdx"};
+        int n =  2;
+
+        String[] answer = new String[strings.length];
+        LinkedList<String> list = new LinkedList<>();
+
+        for (int i = 0; i < strings.length; i++) {
+            list.add(strings[i].charAt(n)+ strings[i]);
+        }
+
+        Collections.sort(list);
+
+        for(int i=0; i<list.size(); i++) {
+            answer[i]=list.get(i).substring(1);
+        }
+
+        System.out.println(Arrays.toString(answer));
+    }
+}
