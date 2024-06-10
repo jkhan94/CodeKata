ca2aad2fc11fcc391fed8cccdd1197e433ee7782
jkhan94 <hjkwork@naver.com>
2024-06-10 16:21:22 +0900

    Add : 가장 가까운 같은 글자
    https://school.programmers.co.kr/learn/courses/30/lessons/142086

--- src/ClosestCharacter.java
+++ src/ClosestCharacter.java
@@ -1,9 +1,11 @@
+import java.util.HashMap;
+
 public class ClosestCharacter {
     public static void main(String[] args) {
         String s = "banana"; // 012345
-        int[] answer = new int[s.length()];
+        int[] answer = new int[s.length()]; //-1 -1 -1 2 2 2
 
-        for (int i = 0; i < s.length(); i++) {
+        /*for (int i = 0; i < s.length(); i++) {
             for (int j = i - 1; j >= 0; j--) {
                 if (s.charAt(i) == s.charAt(j)) {
                     answer[i] = i - j;
@@ -11,11 +13,18 @@ public class ClosestCharacter {
                 }
             }
         }
-
         for (int i = 0; i < answer.length; i++) {
             if (answer[i] == 0) {
                 answer[i] = -1;
             }
+        }*/
+
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i=0; i<s.length();i++){
+            char ch = s.charAt(i);
+            Integer a = map.getOrDefault(ch,i+1);
+            answer[i] = i-map.getOrDefault(ch,i+1);
+            map.put(ch,i);
         }
 
         for (int item : answer) {
