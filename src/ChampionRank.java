ac6e0cf15fb7b0cf1281705f4bb25a538a019a69
jkhan94 <hjkwork@naver.com>
2024-06-12 23:42:11 +0900

    Add : 명예의전당
    https://school.programmers.co.kr/learn/courses/30/lessons/138477

---
+++ src/ChampionRank.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class ChampionRank {
+    /*
+    * 내림차순 정렬
+    k일째 까지는 마지막 점수
+    k이후부터는 k번째 점수*/
+    public static void main(String[] args) {
+        int k = 4;
+        int[] score = {0, 300, 40, 300, 20, 70, 150, 50, 500, 1000};
+
+        int[] answer = new int[score.length];
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        for (int i = 0; i < score.length; i++) {
+            if (i < k) {
+                priorityQueue.add(score[i]);
+                answer[i] = priorityQueue.peek();
+            } else {
+                if (priorityQueue.peek() < score[i]) {
+                    priorityQueue.remove();
+                    priorityQueue.add(score[i]);
+                }
+                answer[i] = priorityQueue.peek();
+            }
+        }
+
+        System.out.println(Arrays.toString(answer));
+    }
+
+}
