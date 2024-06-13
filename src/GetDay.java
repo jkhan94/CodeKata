62a8e958a58e8e2764ce9edcfa1b4ab2ba2d3e1b
jkhan94 <hjkwork@naver.com>
2024-06-13 12:45:52 +0900

    Add : 2016년
    https://school.programmers.co.kr/learn/courses/30/lessons/12901

---
+++ src/GetDay.java
@@ -0,0 +1,26 @@
+public class GetDay {
+    // 1/1 금요일
+    // 윤년이라 2월 29일 총 366일
+    // 31일: 1 3 5 7 8 10 12
+    // 인덱스 1이면 금요일
+    // 2/1 월요일
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 31;
+
+        String answer = "";
+        String[] day = {"THU","FRI","SAT","SUN","MON","TUE","WED"};
+        int[] month = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int index=0;
+
+        for(int i=0;i<a-1;i++) {
+            index += month[i];
+        }
+        index += b;
+        index %= 7;
+        answer = day[index];
+
+        System.out.println(answer);
+
+    }
+}
