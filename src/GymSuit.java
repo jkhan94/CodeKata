6aec25904c9ab2def29c9fcb4b94d61f4d8b6a9b
jkhan94 <hjkwork@naver.com>
2024-06-25 10:50:17 +0900

    refactor : 체육복
    https://school.programmers.co.kr/learn/courses/30/lessons/42862

---
+++ src/GymSuit.java
@@ -0,0 +1,27 @@
+public class GymSuit {
+    public static void main(String[] args){
+        int n =5;
+        int[] lost={2,4};
+        int[] reserve={1, 3, 5};
+        int[] people = new int[n];
+        int answer = n;
+
+        for (int l : lost)
+            people[l-1]--;
+        for (int r : reserve)
+            people[r-1]++;
+
+        for (int i = 0; i < people.length; i++) {
+            if(people[i] == -1) {
+                if(i-1>=0 && people[i-1] == 1) {
+                    people[i]++;
+                    people[i-1]--;
+                }else if(i+1< people.length && people[i+1] == 1) {
+                    people[i]++;
+                    people[i+1]--;
+                }else
+                    answer--;
+            }
+        }
+    }
+}
