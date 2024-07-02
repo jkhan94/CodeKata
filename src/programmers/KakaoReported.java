24657839ae45faf57efc8e028acbf2e6ad9f0ce1
jkhan94 <hjkwork@naver.com>
2024-07-02 21:27:58 +0900

    fix : 신고 결과 받기
    list > map으로 변경
    
    https://school.programmers.co.kr/learn/courses/30/lessons/92334#

--- src/programmers/KakaoReported.java
+++ src/programmers/KakaoReported.java
@@ -11,35 +11,31 @@ public class KakaoReported {
         int[] answer = new int[id_list.length];
         String reporter = "";
         String reported = "";
-        int idx = 0;
 
-        Map<String, Integer> reportedMap = new HashMap<>();
-        List<String> reporters = new ArrayList<>();
-        List<String> reporteds = new ArrayList<>();
+        Map<String, HashSet<String>> reportedMap = new HashMap<>();
+        HashSet<String> reporterSet = new HashSet<>();
 
-        for (int i = 0; i < report.length; i++) {
-            reporter = report[i].split(" ")[0];
-            reported = report[i].split(" ")[1];
-
-            if (reporters.contains(reporter) && reporteds.contains(reported)
-                    && reporteds.get(reporters.indexOf(reporter)).equals(reported)) {
-                continue;
-            } else {
-                reporters.add(reporter);
-                reporteds.add(reported);
-            }
+        for(int i=0; i<id_list.length;i++){
+            reportedMap.put(id_list[i],new HashSet<String>());
         }
 
-        System.out.println(reporters);
-        System.out.println(reporteds);
+        // 당한자, 신고자들
+        for(int i=0; i<report.length;i++){
+            reporter = report[i].split(" ")[0];
+            reported = report[i].split(" ")[1];
+            reportedMap.get(reported).add(reporter);
+        }
 
-        for (int i = 0; i < id_list.length; i++) {
-            if (Collections.frequency(reporteds, id_list[i]) >= k) {
-                for (int j = 0; j < reporteds.size(); j++) {
-                    if (reporteds.get(j).equals(id_list[i])) {
-                        idx = Arrays.asList(id_list).indexOf(reporters.get(j));
-                        answer[idx]++;
-                    }
+        // System.out.println(reportedMap);
+
+        // set의 크기가 k보다 크면 정지
+        // set에 저장된 유저들의 이메일 수++
+        for(int i=0; i<id_list.length;i++){
+            reporterSet = reportedMap.get(id_list[i]);
+            if(reporterSet.size() >= k){
+                // System.out.println(id_list[i]+":"+reporterSet);
+                for(String name : reporterSet){
+                    answer[Arrays.asList(id_list).indexOf(name)]++;
                 }
             }
         }
