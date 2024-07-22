99892d7368b178b0899dd045f4bea0c6b3f30ee0
jkhan94 <hjkwork@naver.com>
2024-07-22 17:03:30 +0900

    add : 타겟 넘버
    https://school.programmers.co.kr/learn/courses/30/lessons/43165

--- src/programmers/TargetNumber.java
+++ src/programmers/TargetNumber.java
@@ -8,7 +8,8 @@ public class TargetNumber {
         int target = 2;
         int[] calc = new int[numbers.length];
 
-        dfs(0, 0, numbers, target);
+//        dfs(0, 0, numbers, target);
+        answer = dfs2(numbers, 0, 0, target);
 
         System.out.println(answer);
     }
@@ -25,5 +26,15 @@ public class TargetNumber {
 
         dfs(depth + 1, sum - numbers[depth], numbers, target);
         dfs(depth + 1, sum + numbers[depth], numbers, target);
+    }
+
+    private static int dfs2(int[] numbers, int n, int sum, int target) {
+        if(n == numbers.length) {
+            if(sum == target) {
+                return 1;
+            }
+            return 0;
+        }
+        return dfs2(numbers, n + 1, sum + numbers[n], target) + dfs2(numbers, n + 1, sum - numbers[n], target);
     }
 }
