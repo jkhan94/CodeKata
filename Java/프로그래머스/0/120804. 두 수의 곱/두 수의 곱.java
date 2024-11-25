37e0503bfc1a2779662a4d16011a163f59ec1afa
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2024-11-25 20:12:21 +0900

    [level 0] Title: 두 수의 곱, Time: 0.04 ms, Memory: 85.2 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120804. 두 수의 곱/두 수의 곱.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        //입력값 체크
+        if(check(num1) && check(num2)){
+            answer=num1*num2;
+        }
+        return answer;
+    }
+    
+    private boolean check(int num){
+        return num>=0 && num<=100;
+    }
+}
