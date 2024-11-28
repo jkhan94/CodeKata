024c8680549a9c655bb6273d4637c3583d9fb9b9
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2024-11-29 01:56:13 +0900

    [level 1] Title: 상위 n개 레코드, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/1/59405. 상위 n개 레코드/상위 n개 레코드.sql
+++ MySQL/프로그래머스/1/59405. 상위 n개 레코드/상위 n개 레코드.sql
@@ -1,4 +1,5 @@
--- 코드를 입력하세요
+-- 오라클: SELECT NAME FROM (SELECT * FROM ANIMAL_INS ORDER BY DATETIME) WHERE rownum = 1;
+-- MySQL: limit a, b : a~b까지 출력
 SELECT name
 from animal_ins
 order by datetime
