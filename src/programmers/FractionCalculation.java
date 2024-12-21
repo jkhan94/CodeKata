2f1121f2947108b1f8dcc779cb5e1e0883c514d8
jkhan94 <hjkwork@naver.com>
2024-12-21 17:15:55 +0900

    add : 분수 덧셈
    https://school.programmers.co.kr/learn/courses/30/lessons/120808

---
+++ src/programmers/FractionCalculation.java
@@ -0,0 +1,53 @@
+package programmers;
+
+/*
+유클리드 호제법
+a%b=r1
+b%r1=r2
+r1%r2=0 이면 r2가 GCD
+
+LCM은 LCM = a'*b'*r2
+a*b = a'*r2*b'*r2
+LCM = a*b/r2
+*/public class FractionCalculation {
+    public static void main(String[] args) {
+        int[] answer = solution(5,2,5,2);
+        System.out.println(answer);
+    }
+    public static int[] solution(int numer1, int denom1, int numer2, int denom2) {
+// 1. 공통 분모 만들기. 최대공배수.
+        int gcd = getGcd(denom1, denom2);
+        int lcm = getLcm(denom1, denom2, gcd);
+        int denom = lcm;
+
+// 2. 분자 더하기
+        int numer = numer1 * lcm / denom1 + numer2 * lcm / denom2;
+// 3. 분자, 분모 약분되는지 확인하기 최대공약수
+        gcd = getGcd(numer, denom);
+        if (gcd != 1) {
+            numer /= gcd;
+            denom /= gcd;
+        }
+
+        int[] answer = {numer, denom};
+        return answer;
+    }
+
+    private static int getGcd(int numer, int denom) {
+        int r = numer % denom;
+        if(r == 0){
+            return denom;
+        }
+        int temp = 0;
+        while (r > 0) {
+            temp = r;
+            r = denom % r;
+            denom = temp;
+        }
+        return temp;
+    }
+
+    private static int getLcm(int numer, int denom, int gcd) {
+        return numer * denom / gcd;
+    }
+}
