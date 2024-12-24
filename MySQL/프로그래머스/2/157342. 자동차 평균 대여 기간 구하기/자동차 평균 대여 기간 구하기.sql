6f63fed3c6cf388a52b12e1f4fb246c0aa34ebd0
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2024-12-24 18:04:16 +0900

    [level 2] Title: 자동차 평균 대여 기간 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ MySQL/프로그래머스/2/157342. 자동차 평균 대여 기간 구하기/자동차 평균 대여 기간 구하기.sql
@@ -0,0 +1,10 @@
+# select *, datediff(end_date, start_date)+1
+# from CAR_RENTAL_COMPANY_RENTAL_HISTORY
+# order by car_id desc
+
+
+SELECT car_id, round(avg(datediff(end_date, start_date)+1),1) as AVERAGE_DURATION
+from CAR_RENTAL_COMPANY_RENTAL_HISTORY
+group by car_id
+having AVERAGE_DURATION>=7
+order by AVERAGE_DURATION desc, car_id desc
