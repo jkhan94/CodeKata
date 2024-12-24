f2a225a2be686459696767158cbf934eb8ef50fb
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2024-12-24 18:01:48 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ MySQL/프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -0,0 +1,7 @@
+SELECT *
+from places
+where host_id in (SELECT host_id
+                    from places
+                    group by host_id
+                    having count(host_id)>=2)
+order by id
