767912d891c6af115cdcd58639fd36230327f1f1
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2024-12-30 02:29:17 +0900

    [level 0] Title: 세균 증식, Time: 0.04 ms, Memory: 81.7 MB -BaekjoonHub

--- Java/프로그래머스/0/120910. 세균 증식/세균 증식.java
+++ Java/프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -3,6 +3,7 @@ n * 2^t
 */
 class Solution {
     public int solution(int n, int t) {
-        return (int)(n * Math.pow(2,t));
+        return (int)(n * Math.pow(2,t));        
+        // return n << t; // 왼쪽 시프트는 *2. t번 밀면 됨
     }
 }
