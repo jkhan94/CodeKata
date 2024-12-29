04fe0c64e824a64f1da86c82b100cba77288120a
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2024-12-30 01:37:50 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,16 @@
+/*
+인원수 / 조각수 = 몫 ... 나머지
+나머지=0이면 판수 = 몫 
+나머지 != 0 이면 판수 = 몫=1
+*/
+class Solution {
+    public int solution(int slice, int n) {
+        int answer = n/slice; 
+        
+        if (n%slice != 0){
+            answer++;
+        }        
+        
+        return answer;
+    }
+}
