1f9b7d30b5a8944544c3e23dbb577e5ccb472d84
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-02 04:11:48 +0900

    [level 0] Title: 외계행성의 나이, Time: 8.84 ms, Memory: 84.2 MB -BaekjoonHub

--- Java/프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
+++ Java/프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,7 +1,7 @@
 class Solution {
     public String solution(int age) {
         String answer = "";
-        char[] ages = new char[10];
+        char[] ages = new char[10]; // 숫자는 0-9까지
         int ten = 10;
         
         for(int i = 0; i<ages.length; i++){
