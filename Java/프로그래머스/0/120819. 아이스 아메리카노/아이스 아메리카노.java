d83a1c2b163325e4e9a41d891756618548acd7ee
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-02 02:29:42 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 76.5 MB -BaekjoonHub

--- Java/프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ Java/프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -1,11 +1,11 @@
 class Solution {
-    static final int price = 5500; // 커피값
+    static final int PRICE = 5500; // 커피값
         
     public int[] solution(int money) {
         int[] answer = new int [2];
         
-        answer[0] = money / price;
-        answer[1] = money % price;
+        answer[0] = money / PRICE;
+        answer[1] = money % PRICE;
         
         return answer;        
         // return new int[] { money / 5500, money % 5500 };
