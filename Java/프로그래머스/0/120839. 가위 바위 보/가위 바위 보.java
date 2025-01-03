24c6cc165756b8f2d794b4d09142927aa1a2490c
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-03 19:37:42 +0900

    [level 0] Title: 가위 바위 보, Time: 16.63 ms, Memory: 92.2 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,30 @@
+/*
+가위 2    0
+바위 0    5
+보   5    2
+*/
+class Solution {
+    private static final String scissor = "2";
+    private static final String rock = "0";
+    private static final String paper = "5";
+    
+    public String solution(String rsp) {
+        String answer = "";
+        String[] rspArray = rsp.split("");
+        for(String r : rspArray){
+            switch(r){
+                case scissor:
+                    answer+=rock;
+                    break;
+                case rock:
+                    answer+=paper;
+                    break;
+                case paper:
+                    answer+=scissor;
+                    break;
+            }
+        }
+        
+        return answer;
+    }
+}
