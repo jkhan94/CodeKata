30b8ccfb6a7f0b993107d59651ace39e3b7d1c1b
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-04 04:25:59 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 89 MB -BaekjoonHub

--- Java/프로그래머스/0/120837. 개미 군단/개미 군단.java
+++ Java/프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -1,6 +1,15 @@
 class Solution {
     private static final int GENERAL = 5; // 장군개미
     private static final int SOLDIER = 3;// 병정개미
+    
+    public int solution(int hp){
+        return hp / GENERAL + (hp % GENERAL / SOLDIER) + hp % GENERAL % SOLDIER;
+    }
+}
+/*
+class Solution {
+    private static final int GENERAL = 5; // 장군개미
+    private static final int SOLDIER = 3;// 병정개미
     private static final int WORKER = 1;// 일개미
     
     public int solution(int hp) {
@@ -12,6 +21,7 @@ class Solution {
         hp%=SOLDIER;
         answer+=hp/WORKER;
         
-        return answer;
+        return answer;        
     }
 }
+*/
