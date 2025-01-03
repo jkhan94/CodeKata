6931ab1b47d35bbf88567b0a95cc0e95ec92ad51
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-03 19:20:49 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.93 ms, Memory: 75.5 MB -BaekjoonHub

--- Java/프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ Java/프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -1,6 +1,33 @@
+// 우선순위 큐를 사용해 데이터를 입력하면서 내림차순 정렬
+// 큐에서 하나씩 꺼내면서 기존 배열과 값 비교.
+import java.util.*;
+class Solution {
+    public int[] solution(int[] emergency) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        int[] answer = new int[emergency.length];
+        int rank = 1;
+                
+        for(int e : emergency){
+            pq.offer(e); // 내림차순 정렬
+        }       
+        
+        while(!pq.isEmpty()){
+            int temp = pq.poll();
+            for(int i=0; i<emergency.length; i++){
+                if(temp == emergency[i]){
+                    answer[i] = rank;
+                    rank++;
+                    break;
+                }
+            }
+        }           
+        
+        return answer;
+    }
+}
+
 /*
-정렬된 배열의 값과 기존 배열의 값을 비교해서 같으면 정렬된 배열의 인덱스+1을 넣음.
-*/
+// 정렬된 배열의 값과 기존 배열의 값을 비교해서 같으면 정렬된 배열의 인덱스+1을 넣음.
 import java.util.*;
 class Solution {
     public int[] solution(int[] emergency) {
@@ -24,3 +51,4 @@ class Solution {
         return answer;
     }
 }
+*/
