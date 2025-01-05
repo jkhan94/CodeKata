dda2126848dace843c66807de7408f6d5988c094
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-05 19:23:15 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.13 ms, Memory: 72.9 MB -BaekjoonHub

--- Java/프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.java
+++ Java/프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.java
@@ -1,6 +1,9 @@
-// 순서 무관. nCr = n!/(n-r)!*r!
-// 오버플로우 나서 에러 나는 듯
-// (27,11) (30,3)
+/*
+순서 무관. nCr = n!/(n-r)!*r! = nCn-r
+오버플로우 나서 에러 나는 듯. 곰하면서 동시에 나눠야 BigInteger 안 쓸 수 있음.
+
+추가 테스트케이스: (27 11 13037895) (30 3 4060) (13 6 1716)
+*//*
 import java.math.*;
 class Solution {
     public int solution(int balls, int share) {    
@@ -12,4 +15,21 @@ class Solution {
       
         return (int)answer;
     }    
+}
+*/
+
+class Solution {
+    public long solution(int balls, int share) {
+        share = Math.min(balls - share, share); // 계산 횟수 최소화
+
+        if (share == 0) { // 0개 고르면 nC0 = 1
+            return 1;
+        }
+
+        long result = solution(balls - 1, share - 1); // 재귀함수
+        result *= balls;
+        result /= share;
+
+        return result;
+    }
 }
