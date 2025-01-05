41a55ca735f87dad615e05ec63f35ce9b53f2f89
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-06 02:00:00 +0900

    [level 0] Title: 합성수 찾기, Time: 0.05 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        if(n==1 || n== 2 || n==3){
+            answer = 0;
+        } else { // 4 이상부터는 나뉘면 바로 카운트후 다음 수로 이동
+            for(int i=4; i<=n; i++){
+                for(int j=2; j<i; j++){
+                    if(i%j == 0){
+                        answer++;
+                        break;
+                    }
+                    
+                }            
+            }
+        }
+        
+        return answer;
+    }
+}
+/*
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int cnt = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) cnt++; // 약수 개수 셈
+            }
+            if (cnt >= 3) answer++;
+        }
+
+        return answer;
+    }
+}
+*/
