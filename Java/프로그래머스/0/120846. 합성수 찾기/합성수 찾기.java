c99cd2085491dbd7168e019197f073e88e546b97
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-06 02:00:58 +0900

    [level 0] Title: 합성수 찾기, Time: 0.15 ms, Memory: 71.8 MB -BaekjoonHub

--- Java/프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
+++ Java/프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int solution(int n) {
         int answer = 0;
         
@@ -19,7 +19,7 @@ class Solution {
         return answer;
     }
 }
-/*
+*/
 class Solution {
     public int solution(int n) {
         int answer = 0;
@@ -35,4 +35,3 @@ class Solution {
         return answer;
     }
 }
-*/
