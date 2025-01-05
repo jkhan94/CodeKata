aa2501a0364e244b415c9aad550edcaab9aebdff
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-05 19:51:45 +0900

    [level 0] Title: 공 던지기, Time: 0.04 ms, Memory: 80.4 MB -BaekjoonHub

--- Java/프로그래머스/0/120843. 공 던지기/공 던지기.java
+++ Java/프로그래머스/0/120843. 공 던지기/공 던지기.java
@@ -6,5 +6,7 @@ class Solution {
     public int solution(int[] numbers, int k) {
         int idx = (2*(k-1)) % numbers.length;
         return numbers[idx];
+        // 배열의 값이 인덱스+1 이라서 이렇게 써도 되긴 함.
+        // return (k-1)*2 % numbers.length+1;
     }
 }
