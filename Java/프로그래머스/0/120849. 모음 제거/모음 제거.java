9d8aebdc8227a7ac93633a386c376dc6f6ca41ad
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-07 00:53:16 +0900

    [level 0] Title: 모음 제거, Time: 1.74 ms, Memory: 75.9 MB -BaekjoonHub

--- Java/프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ Java/프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,17 +1,17 @@
 class Solution {
     public String solution(String my_string) {
-        // String answer = "";
-        // String[] my = my_string.split("");
-        // for(int i=0; i<my.length; i++){
-        //     if(my[i].equals("a") || my[i].equals("e") || my[i].equals("i") ||
-        //       my[i].equals("o") || my[i].equals("u")){
-        //         continue;
-        //     }
-        //     answer += my[i];
-        // }
-        // return answer;
+        String answer = "";
+        String[] my = my_string.split("");
+        for(int i=0; i<my.length; i++){
+            if(my[i].equals("a") || my[i].equals("e") || my[i].equals("i") ||
+              my[i].equals("o") || my[i].equals("u")){
+                continue;
+            }
+            answer += my[i];
+        }
+        return answer;
         
-         return my_string.replaceAll("[aeiou]", "");
+         // return my_string.replaceAll("[aeiou]", "");
 
     }
 }
