1aff28aee82593fb737f3e84e984d31f982f5f90
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-07 01:02:27 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.55 ms, Memory: 82.7 MB -BaekjoonHub

--- Java/프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
+++ Java/프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -7,9 +7,12 @@ class Solution {
         
         for(int i=0; i<my.length; i++){
             answer[i]=Integer.parseInt(my[i]);
+            //  answer[i] = my_string.charAt(i) - '0'; // 문자0을 빼서 정수화
         }        
         Arrays.sort(answer);                
         
         return answer;
-    }
+        
+        //  return Arrays.stream(myString.replaceAll("[A-Z|a-z]", "").split("")).sorted().mapToInt(Integer::parseInt).toArray();
+    }    
 }
