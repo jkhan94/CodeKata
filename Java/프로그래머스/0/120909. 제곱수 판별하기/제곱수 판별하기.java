4ad0a24162faf207dc1f88827d1f04c3dc503c3a
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-07 00:44:04 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 89 MB -BaekjoonHub

--- Java/프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ Java/프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -1,11 +1,11 @@
 class Solution {
     public int solution(int n) {
-        // if(Math.sqrt(n)%1 == 0.0){
-        //     return 1;
-        // } else {
-        //     return 2;
-        // }
+        if(Math.sqrt(n)%1 == 0.0){
+            return 1;
+        } else {
+            return 2;
+        }
 
-        return Math.sqrt(n) % 1 == 0 ? 1 : 2;
+        // return Math.sqrt(n) % 1 == 0 ? 1 : 2;
     }
 }
