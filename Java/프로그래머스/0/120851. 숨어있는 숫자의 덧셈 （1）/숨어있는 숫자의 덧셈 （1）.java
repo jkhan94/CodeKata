ea2fa69b538232c63962f5f44d4de42908c9fe20
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-07 01:08:51 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.40 ms, Memory: 88.3 MB -BaekjoonHub

--- Java/프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
+++ Java/프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -1,10 +1,9 @@
 class Solution {
     public int solution(String my_string) {
         int answer = 0;
-        String[] my = my_string.replaceAll("[^1-9]","").split(""); // 자연수만 남김
-        
-        for(int i=0; i<my.length; i++){
-            answer += Integer.parseInt(my[i]);
+        String my = my_string.replaceAll("[^1-9]",""); // 자연수만 남김
+        for(int i=0; i<my.length(); i++){
+            answer += my.charAt(i) - '0';
         }
         return answer;
     }
@@ -13,9 +12,10 @@ class Solution {
 class Solution {
     public int solution(String my_string) {
         int answer = 0;
-        String my = my_string.replaceAll("[^1-9]",""); // 자연수만 남김
-        for(int i=0; i<my.length(); i++){
-            answer += my.charAt(i) - '0';
+        String[] my = my_string.replaceAll("[^1-9]","").split(""); // 자연수만 남김
+        
+        for(int i=0; i<my.length; i++){
+            answer += Integer.parseInt(my[i]);
         }
         return answer;
     }
