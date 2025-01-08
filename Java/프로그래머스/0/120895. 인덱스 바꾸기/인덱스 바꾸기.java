daac17254ef3ac049e73410b156eab6e103bb7fa
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-08 18:00:32 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 85.4 MB -BaekjoonHub

--- Java/프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
+++ Java/프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -1,12 +1,13 @@
 import java.util.*;
 class Solution {
     public String solution(String my_string, int num1, int num2) {
-        String answer = "";
         char[] my = my_string.toCharArray();
         
-        char temp = my[num2];
-        my[num2] = my[num1];
-        my[num1]=temp;
+        // char temp = my[num2];
+        // my[num2] = my[num1];
+        // my[num1]=temp;        
+        my[num1] = my_string.charAt(num2);
+        my[num2] = my_string.charAt(num1);
         
         // return String.valueOf(my); 
         return new String(my);
