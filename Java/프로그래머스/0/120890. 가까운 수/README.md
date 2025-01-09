82b2fb849706e99650cc814435958752d29a08fa
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-09 19:10:13 +0900

    [level 0] Title: 가까운 수, Time: 1.79 ms, Memory: 88.3 MB -BaekjoonHub

--- Java/프로그래머스/0/120890. 가까운 수/README.md
+++ Java/프로그래머스/0/120890. 가까운 수/README.md
@@ -1,10 +1,10 @@
 # [level 0] 가까운 수 - 120890 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120890) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120890#) 
 
 ### 성능 요약
 
-메모리: 75.3 MB, 시간: 0.06 ms
+메모리: 88.3 MB, 시간: 1.79 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 09일 19:05:29
+2025년 01월 09일 19:10:11
 
 ### 문제 설명
 
--- Java/프로그래머스/0/120890. 가까운 수/가까운 수.java
+++ Java/프로그래머스/0/120890. 가까운 수/가까운 수.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public int solution(int[] array, int n) {
         int answer = 0;
@@ -15,5 +16,21 @@ class Solution {
         }
         
         return answer;
+    }
+}
+*/
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array, int n) {
+        Arrays.sort(array);
+
+        for(int i = 1; i < array.length; i++){
+            if(Math.abs(n-array[0]) > Math.abs(n-array[i])){
+                array[0] = array[i];
+            }
+        }
+        
+        return array[0];
     }
 }
