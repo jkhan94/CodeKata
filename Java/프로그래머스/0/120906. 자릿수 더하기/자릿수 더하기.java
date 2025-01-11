564340f0c634bb45ad8dcde913c0e398b4564e3a
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-11 20:41:39 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 80.2 MB -BaekjoonHub

--- Java/프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
+++ Java/프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public int solution(int n) {
         int answer = 0;
@@ -5,6 +6,19 @@ class Solution {
         
         for(int i=0; i<s.length; i++){
             answer+= Integer.parseInt(s[i]);
+        }
+        
+        return answer;
+    }
+}
+*/
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        while(n>0){
+            answer += n%10;
+            n/=10;
         }
         
         return answer;
