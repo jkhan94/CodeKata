29654d67aeca45b90facd3107d65de4bc3cd6520
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-11 20:49:07 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.03 ms, Memory: 72.5 MB -BaekjoonHub

--- Java/프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.java
+++ Java/프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -1,3 +1,25 @@
+class Solution {
+    public int[] solution(int n, int[] numlist) {
+        int count = 0;
+        for(int i : numlist){
+            if(i%n==0){
+                count++;
+            }
+        }
+
+        int[] answer = new int[count];
+        int idx = 0;
+        for(int i : numlist){
+            if(i%n==0){
+                answer[idx]=i;
+                idx++;
+            }
+        }
+
+
+        return answer;
+    }
+}
 /*
 import java.util.*;
 class Solution {
@@ -14,9 +36,11 @@ class Solution {
     }
 }
 */
+/*
 import java.util.*;
 class Solution {
     public int[] solution(int n, int[] numList) {
         return Arrays.stream(numList).filter(value -> value % n == 0).toArray();
     }
 }
+*/
