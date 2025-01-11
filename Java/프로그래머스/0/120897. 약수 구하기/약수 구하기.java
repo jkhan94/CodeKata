c9bb9c8c904170ce3635a668aeab33deee75b8de
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-11 18:35:10 +0900

    [level 0] Title: 약수 구하기, Time: 3.08 ms, Memory: 86.2 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,30 @@
+/*
+1. for문으로 1~n까지 나머지 0인 걸 찾음
+2. 제곱근 활용
+    1 2 루트8 = 2.82 4 8
+    1 3(루트9) 9
+    1 2 루트10=3.16 5 10
+    
+테스트케이스 추가
+9 
+[1,3,9]
+*/
+import java.util.*;
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> answer = new ArrayList<>();
+        
+        for(int i=1; i<=Math.sqrt(n); i++){
+            if(n%i == 0){
+                answer.add(i);
+                if(!answer.contains(n/i)){
+                    answer.add(n/i);
+                }
+            }
+        }        
+        
+        answer.sort(Comparator.naturalOrder());
+        
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
