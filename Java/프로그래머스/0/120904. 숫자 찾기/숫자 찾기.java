2dc4a01cf5b007fd22d64001cea344b4ee636f5b
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-11 20:38:27 +0900

    [level 0] Title: 숫자 찾기, Time: 8.07 ms, Memory: 79.9 MB -BaekjoonHub

--- Java/프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ Java/프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public int solution(int num, int k) {
         String s = String.valueOf(num);
@@ -9,6 +10,13 @@ class Solution {
             answer = -1;
         }
         
-        return answer;
+        return answer;        
+    }    
+}
+*/
+class Solution {
+    public int solution(int num, int k) {
+        // num 앞에 문자를 추가해서 문자열로 변경 + 인덱스를 1부터 시작하게 함.    
+        return ("-" + num).indexOf(String.valueOf(k));
     }
 }
