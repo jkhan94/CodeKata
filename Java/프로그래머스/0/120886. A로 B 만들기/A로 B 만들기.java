521aba2c442499e9f999730317bf2bfdf95931bf
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-13 18:49:24 +0900

    [level 0] Title: A로 B 만들기, Time: 0.28 ms, Memory: 88.8 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
@@ -0,0 +1,29 @@
+/*
+알파벳 개수가 동일하면 before -> after 변경 가능
+
+1. after에 있는 것이 before에 있으면 replaceFirst 하거나
+2. 개수를 각각 센 다음 알파벳 개수별로 비교
+    => 2번이 나을 듯
+*/
+class Solution {
+    public int solution(String before, String after) {
+        int[] bef = new int[26];
+        int[] aft = new int[26];
+        
+        for(int i=0; i<before.length(); i++){
+            bef[before.charAt(i) - 'a']++;
+        }
+        
+        for(int i=0; i<after.length(); i++){
+            aft[after.charAt(i) - 'a']++;
+        }
+               
+        for(int i=0; i<aft.length; i++){
+            if(aft[i] != bef[i]){
+                return 0;
+            }
+        }
+        
+        return 1;
+    }
+}
