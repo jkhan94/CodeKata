f96a2addc908557c63263329a4468098c5ef585b
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-13 19:21:44 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.77 ms, Memory: 92.6 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -0,0 +1,18 @@
+/*
+숫자만 남긴다. 문자를 전부 공백으로 치환.
+[^0-9] = [^\\d] = [\\D] = 숫자가 아니면
+*/
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        String[] my = my_string.replaceAll("[\\D]"," ").split(" ");
+        
+        for(String m:my){
+            if(m.matches("\\d+")){
+                answer += Integer.parseInt(m);
+            }
+        }
+        
+        return answer;
+    }
+}
