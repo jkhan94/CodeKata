838bc6e254bbedd2b3d33986abe1cf316269fb6d
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-13 19:52:31 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.02 ms, Memory: 69.6 MB -BaekjoonHub

--- Java/프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.java
+++ Java/프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.java
@@ -9,6 +9,7 @@ substring(int start, int end)   start 부터 end-1까지 리턴
 35 1
 68 2
 */
+/*
 class Solution {
     public String[] solution(String my_str, int n) {
         int length = my_str.length()%n == 0? my_str.length()/n : my_str.length()/n+1;
@@ -26,6 +27,24 @@ class Solution {
         
         System.out.println(length);
         
+        return answer;
+    }
+}
+*/
+class Solution {
+    public String[] solution(String my_str, int n) {
+        // 나머지 최대값인 n-1을 더해서 나머지가 있을 경우 올림
+        // 나누어 떨어지면 0 + n-1 이므로 몫은 변화 없음
+        // 나머지가 있으면 r + n-1이므로 1 + (r-1)/n. 즉 몫+1.
+        int length = (my_str.length() + n-1) / n;
+        String[] answer = new String[length];
+
+        for (int i = 0; i < length; i++) {
+            int start = n * i;
+            int end = start + n >= my_str.length()? my_str.length(): start + n;
+            answer[i] = my_str.substring(start, end);
+        }
+
         return answer;
     }
 }
