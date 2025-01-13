b7fdc7771cc89be0521da3e62bb23c094f7f64cd
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-13 18:16:12 +0900

    [level 0] Title: k의 개수, Time: 5042.95 ms, Memory: 375 MB -BaekjoonHub

--- Java/프로그래머스/0/120887. k의 개수/k의 개수.java
+++ Java/프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -1,6 +1,7 @@
 /*
 i~j까지 문자열로 저장해서 "k"가 몇 번 있는지 셈.
 */
+/*
 class Solution {
     public int solution(int i, int j, int k) {
         int answer = 0;
@@ -21,3 +22,32 @@ class Solution {
         return answer;
     }
 }
+*/
+class Solution {
+    public int solution(int i, int j, int k) {
+        String str = "";
+        for(int a = i; a <= j; a++) {
+            str += a+"";
+        }
+
+        // 전체 개수 - k를 제외한 개수
+        return str.length() - str.replace(k+"", "").length();
+    }
+}
+/*
+class Solution {
+    public int solution(int i, int j, int k) {
+        int answer = 0;
+
+        for (int num = i; num <= j; num++){
+            int tmp = num;
+            while (tmp != 0){
+                if (tmp % 10 == k)
+                    answer++;
+                tmp /= 10;
+            }
+        }
+        return answer;
+    }
+}
+*/
