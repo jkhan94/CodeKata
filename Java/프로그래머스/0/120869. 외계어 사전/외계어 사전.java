9f246a0e9ba971c589239ac5f94082658cc51c83
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-14 16:11:06 +0900

    [level 0] Title: 외계어 사전, Time: 0.27 ms, Memory: 86.5 MB -BaekjoonHub

--- Java/프로그래머스/0/120869. 외계어 사전/외계어 사전.java
+++ Java/프로그래머스/0/120869. 외계어 사전/외계어 사전.java
@@ -4,6 +4,7 @@
 Arrays.toString(temp) : [d, e, f]
 String.join("", temp) : dos
 */
+/*
 import java.util.*;
 class Solution {
     public int solution(String[] spell, String[] dic) {
@@ -23,5 +24,25 @@ class Solution {
         }
         
         return answer;
+    }
+}
+*/
+class Solution {
+    public int solution(String[] spell, String[] dic) {
+        for(int i=0;i<dic.length;i++){
+            int answer = 0;
+            
+            // 단어별로 spell 포함하면 +1
+            for(int j=0;j<spell.length;j++){
+                if(dic[i].contains(spell[j])) answer ++;
+            }
+            
+            // spell의 길이와 answer가 동일하다면 spell 모두 포함
+            if(answer==spell.length){ 
+                return 1;
+            }
+        }
+        
+        return 2;
     }
 }
