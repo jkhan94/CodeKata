6ef99c4ee17e3f9049860b791f3a21fec4508f9f
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-14 15:27:38 +0900

    [level 0] Title: OX퀴즈, Time: 0.20 ms, Memory: 76.5 MB -BaekjoonHub

--- Java/프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
+++ Java/프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
@@ -1,6 +1,7 @@
 /*
 유사 문제: https://school.programmers.co.kr/learn/courses/30/lessons/120902
 */
+/*
 class Solution {
     public String[] solution(String[] quiz) {
         String[] answer = new String[quiz.length];
@@ -27,5 +28,16 @@ class Solution {
             
         }
         return answer;
+    }
+}
+*/
+class Solution {
+    public String[] solution(String[] quiz) {
+        for(int i=0; i<quiz.length; i++){
+            String[] text = quiz[i].split(" ");
+            int result = Integer.parseInt(text[0]) + (Integer.parseInt(text[2]) * (text[1].equals("+") ? 1:-1));
+            quiz[i] = result == Integer.parseInt(text[4])? "O": "X";
+        }
+        return quiz;
     }
 }
