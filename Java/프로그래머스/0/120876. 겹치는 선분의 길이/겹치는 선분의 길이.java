773e92f739878295d0369f3d44d78769b38d078d
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-17 01:34:36 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 2.61 ms, Memory: 72 MB -BaekjoonHub

--- Java/프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
+++ Java/프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -3,6 +3,7 @@
 각 선분별로 지나가는 점을 카운트, 카운트 횟수가 1 초과면 중복구간이므로 answer++
 -100 ~ 100이므로 총 200칸
 */
+/*
 class Solution {
     public int solution(int[][] lines) {
         int answer = 0;
@@ -21,5 +22,26 @@ class Solution {
         }        
         
         return answer;
+    }
+}
+*/
+import java.util.*;
+class Solution {
+    public int solution(int[][] lines) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int[] line : lines) {
+            // 선분의 시작점, 끝점 정의
+            int from = Math.min(line[0], line[1]);
+            int to = Math.max(line[0], line[1]);
+            
+            // 선분의 [from, to) 구간(포함-미포함)을 반복
+            for (int i = from; i < to; i++) {
+                // map의 i 위치에 대해 값이 존재하면 1을 더하고, 없으면 1로 초기화
+                map.merge(i, 1, Integer::sum);
+            }
+        }
+
+        // map에서 값이 2 이상인 좌표만 세서 반환
+        return (int) map.values().stream().filter(i -> i > 1).count();
     }
 }
