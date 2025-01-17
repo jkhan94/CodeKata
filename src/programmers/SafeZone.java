b01095aa6841c45daaaa4226a807daf2d4b3caed
jkhan94 <hjkwork@naver.com>
2025-01-17 21:21:43 +0900

    add :  안전지대
    https://school.programmers.co.kr/learn/courses/30/lessons/120866#

---
+++ src/programmers/SafeZone.java
@@ -0,0 +1,50 @@
+package programmers;
+
+public class SafeZone {
+    public static void main(String[] args) {
+        int answer = 0;
+        int[][] board = {{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 1, 0, 0}, {0, 0, 0, 0, 0}};
+        // 상 좌 우 하 좌상 우상 좌하 우하
+        int[] dx = {-1,0,1,0,-1,-1,1,1};
+        int[] dy = {0,-1,0,1,-1,1,-1,1};
+
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board[0].length; j++){
+                if(board[i][j] == 1){
+                    for(int k=0; k<dx.length; k++){
+                        int x = i+dy[k];
+                        int y = j+dx[k];
+                        if(x<0){
+                            x=0;
+                        } else if(x>=board.length){
+                            x=i;
+                        }
+                        if(y<0){
+                            y=0;
+                        } else if(y>=board[0].length){
+                            y=j;
+                        }
+
+                        if(board[x][y] != 1){
+                            board[x][y]--;
+                        }
+
+                    }
+                }
+            }
+        }
+
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board[0].length; j++){
+                 System.out.print(board[i][j]+" ");
+                if(board[i][j] == 0){
+                    answer++;
+                }
+            }
+             System.out.println();
+        }
+
+        System.out.println(answer);
+
+    }
+}
