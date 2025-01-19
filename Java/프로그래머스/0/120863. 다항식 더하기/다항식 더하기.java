3874ca4891aa4129eb1799d7fdfca06d927e11c1
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-20 01:38:51 +0900

    [level 0] Title: 다항식 더하기, Time: 7.43 ms, Memory: 89.3 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120863. 다항식 더하기/다항식 더하기.java
@@ -0,0 +1,47 @@
+/*
+테스트케이스 추가
+"1 + 2 + 3" "6"
+"10x + 11" "10x + 11"
+"x" "x"
+*/
+class Solution {
+    public String solution(String polynomial) {
+        String answer = "";
+        int x = 0;
+        int c = 0;
+        String[] p = polynomial.split(" ");       
+        
+        for(String a : p){
+            if(a.contains("x")){
+                if(a.equals("x")){
+                    x+=1;
+                } else {
+                    x += Integer.parseInt(a.substring(0,a.length()-1));
+                }                
+            } else if(a.matches("^[0-9]*$")) {
+                // + : 1자 이상, * : 0자 이상
+                c += Integer.parseInt(a);
+            }
+        }             
+
+        if(x==0){
+            answer = c+"";
+        } else if(x==1){
+            if(c==0){
+                answer = "x";
+            } else {
+                answer = "x + "+c;
+            }            
+        } else {
+            if(c==0){
+                answer = x+"x";
+            } else {
+                answer = x+"x + "+c;
+            }            
+        }
+        
+        
+                
+        return answer;
+    }
+}
