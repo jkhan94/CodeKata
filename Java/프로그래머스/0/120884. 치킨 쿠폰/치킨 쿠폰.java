d67f8dfa4b422460d8df9e31458e53c50a2c9fb6
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-22 03:09:45 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.02 ms, Memory: 84.8 MB -BaekjoonHub

--- Java/프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
+++ Java/프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -20,11 +20,18 @@ class Solution {
     }
 }
 */
+/*
+첫 10마리 시키면 1마리 공짜 = 쿠폰 1장 있는 상태
+이후부터는 9마리 시킬때마다 1마리 공짜
+
+주어진 닰 수가 9로 나누어떨어지면 맨 처음은 10마리여야 하므로 1마리 빼야 함.
+0마리 시켰을 경우도 chicken/9 = 0 > 0%9=0라서 -1 리턴됨. 따라서 1마리 이상 주문한 경우를 고려해서 -1 해야 함
+*/
 class Solution {
     public int solution(int chicken) {
         int answer = chicken/9;
         
-        if(chicken>1 && chicken%9==0){
+        if(chicken>0 && chicken%9==0){
             answer--;
         }
 
