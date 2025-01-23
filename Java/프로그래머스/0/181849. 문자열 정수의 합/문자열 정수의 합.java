a19a4f389035f07a5a554174ec1476e9f5cdc9dc
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-24 02:33:29 +0900

    [level 0] Title: 문자열 정수의 합, Time: 0.39 ms, Memory: 86.4 MB -BaekjoonHub

--- Java/프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.java
+++ Java/프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.java
@@ -1,3 +1,15 @@
+/*
+자료형	    최대값	                                        최대 자릿수
+byte	    127	                                            3
+short	    32,767	                                        5
+int	        2,147,483,647	                                10
+long	    9,223,372,036,854,775,807	                    19
+float	    약 ±3.40282347 × 10³⁸ (7자리 정밀도)      	    7
+double	    약 ±1.7976931348623157 × 10³⁰⁸ (15자리 정밀도)	15
+
+정수형은 정확한 자릿수 기준
+부동소수점(float, double)은 유효숫자(precision) 기준
+*/
 class Solution {
     public int solution(String num_str) {
         int answer = 0;
