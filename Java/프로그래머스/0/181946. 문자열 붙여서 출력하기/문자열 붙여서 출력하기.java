814a2e00f4c9cd9d40b5f3f39361e14379e3bb2b
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-24 02:12:35 +0900

    [level 0] Title: 문자열 붙여서 출력하기, Time: 179.76 ms, Memory: 71.4 MB -BaekjoonHub

--- Java/프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.java
+++ Java/프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.java
@@ -1,3 +1,8 @@
+/*
+String 불변객체. 값 변경 시 새 객체 추가
+StringBuilder 문자열 변경 시 기존 객체 변경. 싱글스레드에서 유리.
+StringBuffer 문자열 변경 시 기존 객체 변경. 멀티스레드에서 유리.
+*/
 import java.util.Scanner;
 
 public class Solution {
@@ -6,6 +11,8 @@ public class Solution {
         String a = sc.next();
         String b = sc.next();
         
-        System.out.println(a+b);
+        StringBuffer sb = new StringBuffer();
+        
+        System.out.println(sb.append(a).append(b));
     }
 }
