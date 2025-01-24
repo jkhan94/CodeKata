1c6718eb6c3d64e2e979b29e216f9ae69028cde8
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-25 05:22:05 +0900

    [level 0] Title: 배열 조각하기, Time: 0.08 ms, Memory: 89 MB -BaekjoonHub

--- Java/프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
+++ Java/프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
@@ -1,12 +1,36 @@
 /*
-순간적으로 query[i]가 짝수면 뒤를 자르고, 홀수면 앞을 자르라는 건 줄 알았음ㅋㅋ
+순간적으로 query[i]가 짝수면 뒤를 자르고, 홀수면 앞을 자르라는 건 줄 알았음.
+그래서 리스트로 풀었는데 query 인덱스 기준이면 int 두 개로 해결 가능.
 
 테스트케이스 추가
 [0, 1, 2, 3, 4, 5] [4, 0, 2] [0, 1, 2]
 */
+
+class Solution {
+    public int[] solution(int[] arr, int[] query) {
+        int start = 0;
+        int end = arr.length-1;
+        
+        for(int i=0; i<query.length; i++){           
+            if(i%2 == 0){ // even
+                end = start + query[i];
+            } else { // odd
+                start += query[i];
+            }
+        }
+
+        int[] answer = new int[end-start+1];
+        for(int i=start; i<=end; i++){
+            answer[i-start]=arr[i];
+        }
+        
+        return answer;
+    }
+}
+
+/*
 import java.util.*;
 import java.util.stream.Collectors;
-
 class Solution {
     public int[] solution(int[] arr, int[] query) {
         List<Integer> list = Arrays.stream(arr).boxed().collect(Collectors.toList());
@@ -22,3 +46,4 @@ class Solution {
         return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
+*/
