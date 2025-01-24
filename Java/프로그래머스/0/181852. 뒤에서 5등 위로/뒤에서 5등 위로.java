4d0030c53009bfd1475fd604782de593082e2fd6
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-25 02:57:16 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 7.90 ms, Memory: 75.6 MB -BaekjoonHub

--- Java/프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
+++ Java/프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -1,4 +1,5 @@
 import java.util.*;
+/*
 class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[num_list.length-5];
@@ -7,10 +8,25 @@ class Solution {
         
         for(int i=0; i<answer.length; i++){
             answer[i] = num_list[i+5];
-        }
+        }        
+        return answer;
         
-        // return answer;
         return Arrays.copyOfRange(num_list,5,num_list.length);
         // return Arrays.stream(numList).sorted().skip(5).toArray();
+    }
+}
+*/
+/*
+class Solution {
+    public int[] solution(int[] num_list) {        
+        Arrays.sort(num_list); // ASC
+        
+        return Arrays.copyOfRange(num_list,5,num_list.length);
+    }
+}
+*/
+class Solution {
+    public int[] solution(int[] num_list) {
+        return Arrays.stream(num_list).sorted().skip(5).toArray();
     }
 }
