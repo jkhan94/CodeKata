0f2ef6730f50ca4a967c43443db3e19d3f1a6305
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-27 14:46:55 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.04 ms, Memory: 87.8 MB -BaekjoonHub

--- Java/프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ Java/프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -1,3 +1,20 @@
+/*
+replace : 특수문자, 문자 구분해서 전부 치환
+replaceAll : 정규식을 받아 전부 치환.
+
+. : 정규식에선 모든 문자를 의미.
+예) 프로.그래.머스
+replace(".","/") : 프로/그래/머스
+replaceAll(".","/") : ////////
+*/
+class Solution {
+    public int solution(String myString, String pat) {
+        myString = myString.replace("A","T").replace("B","A").replace("T","B");
+     
+        return myString.contains(pat)? 1 : 0;
+    }
+}
+/*
 class Solution {
     public int solution(String myString, String pat) {
         myString = myString.replaceAll("A","T");
@@ -7,3 +24,4 @@ class Solution {
         return myString.indexOf(pat) != -1? 1 : 0;
     }
 }
+*/
