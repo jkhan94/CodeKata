110402486f28f6e5dfc434b34988dfc9aafc3436
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-28 01:54:45 +0900

    [level 0] Title: 배열 만들기 2, Time: 3.13 ms, Memory: 77.7 MB -BaekjoonHub

--- Java/프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
+++ Java/프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -1,6 +1,7 @@
 /*
 0 또는 5로 구성 = 0과 5로 구성되지 않음
 */
+/*
 import java.util.*;
 class Solution {
     public int[] solution(int l, int r) {
@@ -20,5 +21,27 @@ class Solution {
         }
         
         return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
+*/
+
+import java.util.ArrayList;
+class Solution {
+    public int[] solution(int l, int r) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 1; i < 64; i++) {
+            // 0과 5로만 이루어진 숫자를 생성하는 규칙: 이진수의 1을 5로 치환
+            // 이진수 문자열 101을 10진법으로 해석하면 101이 그대로 넘어옴.
+            // 여기에 5를 곱하면 1만 5로 치환해서 0,5로 구성된 숫자가 나옴.
+            int num = Integer.parseInt(Integer.toBinaryString(i))*5;
+
+            if (l <= num && num <= r){
+                list.add(num);
+            }
+        }
+
+        return list.isEmpty()? new int[] { -1 } : list.stream().mapToInt(i -> i).toArray();
     }
 }
