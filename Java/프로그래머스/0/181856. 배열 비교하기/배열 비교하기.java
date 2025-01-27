1432b874016a97a62052df42fb66906158b758d5
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-27 14:19:02 +0900

    [level 0] Title: 배열 비교하기, Time: 0.05 ms, Memory: 74.5 MB -BaekjoonHub

--- Java/프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
+++ Java/프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -1,11 +1,10 @@
 class Solution {
     public int solution(int[] arr1, int[] arr2) {
-        int answer = 0;
         int arr1Len = arr1.length;
         int arr2Len = arr2.length;
         int sum1 = 0;
         int sum2 = 0;
-        
+                
         if(arr1Len == arr2Len){
             for(int a:arr1){
                 sum1+=a;
@@ -14,22 +13,13 @@ class Solution {
                 sum2+=b;
             }
             if(sum1==sum2){
-                answer = 0;
+                return 0;
             } else {
-                if(Math.max(sum1, sum2) == sum1){
-                    answer = 1;
-                } else {
-                    answer = -1;
-                }
+                return Math.max(sum1, sum2) == sum1? 1 : -1;
             }
         } else {
-            if(Math.max(arr1Len, arr2Len) == arr1Len){
-                answer = 1;
-            } else {
-                answer = -1;
-            }
+            return Math.max(arr1Len, arr2Len) == arr1Len? 1 : -1;
         }
-        
-        return answer;
+
     }
 }
