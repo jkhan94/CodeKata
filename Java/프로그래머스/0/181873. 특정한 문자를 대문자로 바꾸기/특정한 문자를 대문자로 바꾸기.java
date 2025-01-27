0ce06cb8e702bb8880c89298aa0df9f8fe2c7579
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-28 02:01:26 +0900

    [level 0] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.51 ms, Memory: 89 MB -BaekjoonHub

--- Java/프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.java
+++ Java/프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.java
@@ -1,6 +1,14 @@
 class Solution {
     public String solution(String my_string, String alp) {
+        String a = alp.toUpperCase();
+        return my_string.replaceAll(alp, a);
+    }
+}
+/*
+class Solution {
+    public String solution(String my_string, String alp) {
         String answer = "";
+        
         for(int i=0; i<my_string.length(); i++){
             if(my_string.charAt(i) == alp.charAt(0)){
                 answer += alp.toUpperCase();
@@ -8,6 +16,8 @@ class Solution {
                 answer += my_string.charAt(i);
             }
         }
+        
         return answer;
     }
 }
+*/
