d9b4a9a63ec448674d53ed92aec370c68b5a00a3
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-28 01:19:58 +0900

    [level 0] Title: ad 제거하기, Time: 1.15 ms, Memory: 88.3 MB -BaekjoonHub

--- Java/프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
+++ Java/프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -1,4 +1,30 @@
 /*
+list.toArray(new String[크기])
+    실제 크기 < 지정된 값보다 작다면 남는 부분은 null
+    실제 크기 > 지정된 값보다 크다면 값을 무시하고 생성
+    보통 0으로 설정하는 것이 관례.
+
+list.toArray(String[]::new) (Java 11 이상)
+    크기를 자동으로 계산해서 생성
+*/
+import java.util.*;
+class Solution {
+    public String[] solution(String[] strArr) {
+        ArrayList<String> answer = new ArrayList<>();
+        int cnt = 0;
+        
+        for(int i=0; i<strArr.length; i++){
+            if(!strArr[i].contains("ad")){
+                answer.add(strArr[i]);
+                cnt++;
+            }
+        }
+        
+        // return answer.toArray(new String[0]);
+        return answer.toArray(String[]::new);
+    }
+}
+/*
 import java.util.*;
 class Solution {
     public String[] solution(String[] strArr) {
@@ -23,20 +49,3 @@ class Solution {
     }
 }
 */
-import java.util.*;
-class Solution {
-    public String[] solution(String[] strArr) {
-        ArrayList<String> answer = new ArrayList<>();
-        int cnt = 0;
-        
-        for(int i=0; i<strArr.length; i++){
-            if(!strArr[i].contains("ad")){
-                answer.add(strArr[i]);
-                cnt++;
-            }
-        }
-        
-        // return answer.toArray(new String[cnt]);
-        return answer.toArray(String[]::new);
-    }
-}
