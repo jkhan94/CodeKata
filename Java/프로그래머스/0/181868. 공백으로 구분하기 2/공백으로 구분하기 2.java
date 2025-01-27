832e2e7265cb9bfeef6dd82579ec34ba57cca46e
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-27 14:53:35 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.29 ms, Memory: 85.3 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String my_string) {
+        String[] my = my_string.split(" ");
+        ArrayList<String> answer = new ArrayList<>();
+        
+        for(int i=0; i<my.length; i++){
+            if(!my[i].equals("")){
+                answer.add(my[i]);
+            }
+        }
+        
+        return answer.toArray(new String[answer.size()]);
+    }
+}
