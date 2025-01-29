bed26eefaf8e7b02d271c87049879661bb3082d8
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-30 02:00:44 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 2.97 ms, Memory: 72.3 MB -BaekjoonHub

--- Java/프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
+++ Java/프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -2,6 +2,24 @@
 배열 쓰면 비교 연산해야 함.
 맵<등수, 번호> 로 저장한 뒤 정렬해서 리턴
 */
+import java.util.PriorityQueue;
+
+import java.util.*;
+class Solution {
+    public int solution(int[] rank, boolean[] attendance) {
+        // rank[i] 기준으로 i 정렬
+        // 최소힙이 디폴트. 최대힙은 (i -> rank[i]).reverse() 해주면 됨.
+        PriorityQueue<Integer> que = new PriorityQueue<>(Comparator.comparingInt(i -> rank[i]));
+        
+        for (int i = 0; i < attendance.length; i++) {
+            if (attendance[i])
+                que.add(i);
+        }
+
+        return que.poll() * 10000 + que.poll() * 100 + que.poll();
+    }
+}
+/*
 import java.util.*;
 class Solution {
     public int solution(int[] rank, boolean[] attendance) {
@@ -19,3 +37,4 @@ class Solution {
         return map.get(keySet.get(0))*10000 + map.get(keySet.get(1))*100+ map.get(keySet.get(2));
     }
 }
+*/
