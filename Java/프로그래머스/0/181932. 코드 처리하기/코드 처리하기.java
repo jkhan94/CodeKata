8428e5e904f3d44f028a79ae44a1d7b714c57e3e
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-30 01:12:49 +0900

    [level 0] Title: 코드 처리하기, Time: 52.18 ms, Memory: 81.8 MB -BaekjoonHub

--- Java/프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ Java/프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -6,13 +6,37 @@ mode=0
 mode=1
     !code[i].equals("1") && i%2==1 => ret += code[i]
     code[i].equals("1") => mode=0
-    
+
+문자가 1이면 mode 변경
+문자가 1이 아니고 인덱스%2=mode면 문자열 뒤에 붙이기
+
 시작 mode=0
 리턴문자가 빈 문자열이면 "EMPTY" 리턴
 
 테스트케이스 추가
 "1" "EMPTY"
 */
+class Solution {
+    public String solution(String code) {
+        StringBuffer answer = new StringBuffer();
+        String[] codes = code.split("");
+        int mode = 0;
+        
+        for(int i=0; i<codes.length; i++){            
+            if(codes[i].equals("1")){
+                mode = mode==0? 1 : 0; 
+                continue;
+            }
+            
+            if(i%2==mode){
+                answer.append(codes[i]);
+            }            
+        }
+        
+        return answer.length()==0? "EMPTY" : answer.toString();
+    }
+}
+/*
 class Solution {
     public String solution(String code) {
         String answer = "";
@@ -45,3 +69,4 @@ class Solution {
         return answer;
     }
 }
+*/
