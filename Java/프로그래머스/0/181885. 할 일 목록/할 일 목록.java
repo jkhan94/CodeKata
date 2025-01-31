6ac90cdac3fa9dcc9bfdc3d39a349f07df645603
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-31 12:55:16 +0900

    [level 0] Title: 할 일 목록, Time: 0.69 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        ArrayList<String> answer = new ArrayList<>();
+        
+        for(int i=0; i<todo_list.length; i++){
+            if(!finished[i]){
+                answer.add(todo_list[i]);
+            }
+        }
+        
+        // 리스트명.toArray(반환할 배열 타입 지정);
+        // list.toArray(new String[0])
+        return answer.toArray(String[]::new); // 자바11 이상
+    }
+}
