bcee66322419b30204c1029cc0c59e35061cd26c
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-02-01 19:09:08 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.04 ms, Memory: 84.9 MB -BaekjoonHub

--- Java/프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
+++ Java/프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public int solution(String my_string, String is_prefix) {
         if (my_string.startsWith(is_prefix)){
@@ -7,16 +8,16 @@ class Solution {
         return 0;
     }
 }
+*/
 
-/*
 // 문자열이 포함되어 있고 && 인덱스가 0이면 접두사
+// 포함되어 있지 않다면 -1 리턴하니까 contains는 불필요.
 class Solution {
     public int solution(String my_string, String is_prefix) {        
-        if(my_string.contains(is_prefix) && my_string.indexOf(is_prefix)==0){
+        if(my_string.indexOf(is_prefix)==0){
             return 1;
         }
         
         return 0;
     }
 }
-*/
