51d965a8e0686066cba836130953c599fc21836e
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-02-01 19:02:06 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.08 ms, Memory: 84.7 MB -BaekjoonHub

--- Java/프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
+++ Java/프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -1,6 +1,19 @@
 class Solution {
     public int[] solution(int n, int k) {
         int[] answer = new int[n/k];
+
+        // 개수 만큼 k의 배수를 저장한 배열 리턴
+        for (int i = 1; i <= n/k; i++) {
+            answer[i - 1] = k * i;
+        }
+
+        return answer;
+    }
+}
+/*
+class Solution {
+    public int[] solution(int n, int k) {
+        int[] answer = new int[n/k];
         
         for(int i=1; i<=n; i++){
             if(i%k == 0){
@@ -11,3 +24,4 @@ class Solution {
         return answer;
     }
 }
+*/
