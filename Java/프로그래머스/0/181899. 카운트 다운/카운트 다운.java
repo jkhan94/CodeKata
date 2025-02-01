907c8e9b08c8a30b22be966646178b010ad517f1
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-02-01 18:56:48 +0900

    [level 0] Title: 카운트 다운, Time: 0.03 ms, Memory: 72.3 MB -BaekjoonHub

--- Java/프로그래머스/0/181899. 카운트 다운/카운트 다운.java
+++ Java/프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -2,6 +2,17 @@ class Solution {
     public int[] solution(int start_num, int end_num) {
         int[] answer = new int[start_num-end_num+1];
         
+        for(int i=0; i<=start_num-end_num; i++) {
+            answer[i] = start_num - i;
+        }
+        return answer;
+    }
+}
+/*
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int[] answer = new int[start_num-end_num+1];
+        
         for(int i=start_num; i>=end_num; i--){
             answer[start_num-i] = i;
         }
@@ -9,3 +20,4 @@ class Solution {
         return answer;
     }
 }
+*/
