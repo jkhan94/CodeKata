faadd76d4d2f62a488b065c00768c18a52cda95b
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-02-01 18:05:48 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.03 ms, Memory: 84.8 MB -BaekjoonHub

--- Java/프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
+++ Java/프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -1,5 +1,25 @@
 class Solution {
     public int solution(int[] num_list) {
+        int even = 0;
+        int odd = 0;
+
+        for(int num : num_list) {
+            if(num % 2 == 0) {
+                even *= 10;
+                even += num;
+            } else {
+                odd *= 10;
+                odd += num;
+            }
+        }
+
+        return even + odd;
+    }
+}
+
+/*
+class Solution {
+    public int solution(int[] num_list) {
         StringBuffer odd = new StringBuffer();
         StringBuffer even = new StringBuffer();
         
@@ -14,3 +34,4 @@ class Solution {
         return Integer.parseInt(even.toString()) + Integer.parseInt(odd.toString());
     }
 }
+*/
