019f95cf14796cf4568880012039c0c69eba1bf7
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-02-01 18:18:16 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.04 ms, Memory: 79.4 MB -BaekjoonHub

--- Java/프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
+++ Java/프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -1,11 +1,19 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] num_list, int n) {       
+        return Arrays.copyOfRange(num_list, 0, n);
+    }
+}
+/*
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        int[] answer = new int[n];
+        int[] answer = new int[n];     
         
         for(int i=0; i<n; i++){
             answer[i] = num_list[i];
-        }
+        }        
         
         return answer;
     }
 }
+*/
