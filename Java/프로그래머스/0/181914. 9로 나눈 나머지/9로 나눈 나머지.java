93f6ddc0df212a6c7dc76b293bea994a4930b5e2
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-02-10 03:55:35 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 5.13 ms, Memory: 89.9 MB -BaekjoonHub

--- Java/프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
+++ Java/프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -3,7 +3,8 @@ class Solution {
         int answer = 0;
         
         for(int i=0; i<number.length(); i++){
-            answer+=Character.getNumericValue(number.charAt(i));
+            answer += number.charAt(i) - '0'; //'1'은 아스키코드. 숫자를 원하면 '0'을 빼야 함
+            // answer+=Character.getNumericValue(number.charAt(i));
         }
         return answer%9;
     }
