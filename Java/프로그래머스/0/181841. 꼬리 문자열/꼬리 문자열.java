552667e46799f526068e30ac10162548b9f18a5d
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-02-10 17:47:01 +0900

    [level 0] Title: 꼬리 문자열, Time: 0.15 ms, Memory: 84.7 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String[] str_list, String ex) {
+        StringBuffer answer = new StringBuffer();
+        
+        for(int i=0; i<str_list.length; i++){
+            if(str_list[i].contains(ex)){
+                continue;
+            }
+            answer.append(str_list[i]);
+        }
+        
+        return answer.toString();
+    }
+}
