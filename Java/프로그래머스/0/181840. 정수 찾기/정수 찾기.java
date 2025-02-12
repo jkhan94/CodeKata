709d5786c1ba39bb3c2f9597d2078384b2c23ef8
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-02-13 01:03:51 +0900

    [level 0] Title: 정수 찾기, Time: 0.02 ms, Memory: 69.4 MB -BaekjoonHub

--- Java/프로그래머스/0/181840. 정수 찾기/정수 찾기.java
+++ Java/프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int[] num_list, int n) {
-        for(int i=0; i<num_list.length; i++){
-            if(num_list[i] == n){
+        for(int num : num_list){
+            if(num == n){
                 return 1;
             }
         }        
