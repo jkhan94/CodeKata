bee4f07d88b1f2affa95fa219449bb667ab6796a
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-02-13 01:50:50 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 16.61 ms, Memory: 99.8 MB -BaekjoonHub

--- Java/프로그래머스/0/181866. 문자열 잘라서 정렬하기/README.md
+++ Java/프로그래머스/0/181866. 문자열 잘라서 정렬하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 잘라서 정렬하기 - 181866 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181866) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181866#) 
 
 ### 성능 요약
 
-메모리: 95.9 MB, 시간: 22.61 ms
+메모리: 99.8 MB, 시간: 16.61 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 13일 01:45:57
+2025년 02월 13일 01:50:48
 
 ### 문제 설명
 
--- Java/프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
+++ Java/프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
@@ -6,9 +6,27 @@
 import java.util.*;
 class Solution {
     public String[] solution(String myString) {
+        ArrayList<String> answer = new ArrayList<>();
+        
+        for(String my : myString.split("x")){
+            if(my.isEmpty()){
+                continue;
+            }
+            answer.add(my);
+        }
+        Collections.sort(answer);
+        
+        return answer.toArray(String[]::new);
+    }
+}
+/*
+import java.util.*;
+class Solution {
+    public String[] solution(String myString) {
         return Arrays.stream(myString.split("x")) // "x" 기준으로 분리
                      .filter(s -> !s.isEmpty())   // 빈 문자열 제거
                      .sorted()                    // 오름차순 정렬
                      .toArray(String[]::new);     // 배열로 변환
     }
 }
+*/
