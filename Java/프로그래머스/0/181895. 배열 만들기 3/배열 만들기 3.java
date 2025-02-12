e17b8315e071533a1aff8286dba56dd830416e9b
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-02-13 01:36:41 +0900

    [level 0] Title: 배열 만들기 3, Time: 1.21 ms, Memory: 111 MB -BaekjoonHub

--- Java/프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
+++ Java/프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -1,3 +1,24 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] intervals) {
+        int s1 = intervals[0][0];
+        int e1 = intervals[0][1];
+        int s2 = intervals[1][0];
+        int e2 = intervals[1][1];
+        
+        int[] answer = new int[e1-s1 + e2-s2 +2];
+        int idx=0;
+        
+        for(int i=s1; i<=e1; i++){
+            answer[idx++] = arr[i];            
+        }
+        for(int i=s2; i<=e2; i++){
+            answer[idx++] = arr[i];            
+        }        
+        
+        return answer;
+    }
+}
+/*
 import java.util.*;
 class Solution {
     public int[] solution(int[] arr, int[][] intervals) {
@@ -12,3 +33,4 @@ class Solution {
         return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
+*/
