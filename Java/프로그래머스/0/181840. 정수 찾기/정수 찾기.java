db052fbd7d6231c56bdc2a0219ac5fa88307ca33
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-02-13 01:16:15 +0900

    [level 0] Title: 정수 찾기, Time: 0.10 ms, Memory: 89.9 MB -BaekjoonHub

--- Java/프로그래머스/0/181840. 정수 찾기/정수 찾기.java
+++ Java/프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -1,3 +1,22 @@
+/*
+테스트케이스 추가
+[1, 30, 40], 3 , 0
+contains는 부분 문자열을 찾음. 30에 3이 있어서 1 리턴.
+문자열 대신 리스트의 contains를 쓰면 정확하게 3일 때만 1 리턴.
+*/
+import java.util.*;
+class Solution {
+    public int solution(int[] num_list, int n) {
+        ArrayList<String> ans = new ArrayList<>();
+        
+        for(int num : num_list){
+            ans.add(String.valueOf(num));
+        }
+        
+        return ans.contains(String.valueOf(n))? 1:0;
+    }
+}
+/*
 class Solution {
     public int solution(int[] num_list, int n) {
         for(int num : num_list){
@@ -8,3 +27,4 @@ class Solution {
         return 0;
     }
 }
+*/
