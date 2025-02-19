3d621bab1a013934ecf859a217e61304ca8ac91b
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-02-20 01:59:25 +0900

    [level 0] Title: 빈 배열에 추가, 삭제하기, Time: 3.97 ms, Memory: 85.9 MB -BaekjoonHub

--- Java/프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.java
+++ Java/프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.java
@@ -1,6 +1,27 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] arr, boolean[] flag) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < flag.length; i++) {
+            if (flag[i]) {
+                for (int j = 0; j < arr[i]*2; j++){
+                    stack.push(arr[i]);
+                }
+            } else {
+                for (int j = 0; j < arr[i]; j++){
+                    stack.pop();
+                }
+            }
+        }
+
+        return stack.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
+/*
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr, boolean[] flag) {
         ArrayList<Integer> answer = new ArrayList<>();
         
         for(int i=0; i<arr.length; i++){
@@ -19,3 +40,4 @@ class Solution {
         return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
+*/
