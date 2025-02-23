a6ec04196d95e09cf669822aee2e7281bd7cf300
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-02-23 16:25:55 +0900

    [level 0] Title: 문자열 묶기, Time: 6.22 ms, Memory: 109 MB -BaekjoonHub

--- Java/프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
+++ Java/프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -1,17 +1,14 @@
 class Solution {
     public int solution(String[] strArr) {
         int answer=0;
-        int[] len = new int[strArr.length];
+        int[] len = new int[31];
         
         for(int i=0; i<strArr.length; i++){
             len[strArr[i].length()]++;
         }
         
-        answer = len[0];
-        for(int i=1; i<len.length; i++){
-            if(len[i] > answer){
-                answer = len[i];
-            }
+        for(int i=0; i<len.length; i++){
+            answer = Math.max(len[i], answer);
         }
         
         return answer;
