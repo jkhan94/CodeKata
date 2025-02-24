ea2d66b4d57b56c0707f7e1123cff4bc7cde9b82
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-02-24 17:44:32 +0900

    [level 0] Title: 2의 영역, Time: 3.16 ms, Memory: 90.4 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] solution(int[] arr) {
+        int start = -1;
+        int end = -1;
+        
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] == 2){
+                start = i;
+                break;
+            }
+        }
+        
+        if(start == -1){
+            return new int[] {-1};
+        }
+        
+        for(int i=start; i<arr.length; i++){
+            if(arr[i] == 2){
+                end = i;
+            }
+        }
+        
+        int[] answer = new int[end - start +1];
+        for(int i=start; i<=end; i++){
+            answer[i-start] = arr[i];
+        }
+        
+        return answer;
+    }
+}
