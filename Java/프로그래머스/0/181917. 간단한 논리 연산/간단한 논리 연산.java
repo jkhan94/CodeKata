2e12df398e4549252acfa5c8c751591f9da17ae5
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-02-25 19:04:15 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.02 ms, Memory: 86.9 MB -BaekjoonHub

--- Java/프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.java
+++ Java/프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.java
@@ -1,6 +1,15 @@
-// ∧ AND / ∨ OR
+/*
+∧ AND / ∨ OR
+
+|, & : 비트OR, 비트AND. 
+        양쪽 모두 평가.
+        boolean이면 논리연산, int 같은 정수면 비트 연산
+
+||, && : 논리OR, 논리AND.
+            단축 평가(하나라도 참이나 거짓이면 값 반환) => 성능 최적확
+*/
 class Solution {
     public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
-        return (x1 | x2) & (x3 | x4);
+        return (x1 || x2) && (x3 || x4);
     }
 }
