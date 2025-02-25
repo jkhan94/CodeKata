101b02e8d14f9c3b89a7745d615b1fe8d2aede65
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-02-25 19:14:43 +0900

    [level 0] Title: 커피 심부름, Time: 0.66 ms, Memory: 82.6 MB -BaekjoonHub

--- Java/프로그래머스/0/181837. 커피 심부름/커피 심부름.java
+++ Java/프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -6,6 +6,25 @@ class Solution {
     private static final int AMERICANO = 4500;
     private static final int CAFELATTE = 5000;
     
+    public int solution(String[] order) {      
+        int answer = 0;
+        
+        for(String o : order){
+            if(o.contains("cafelatte")){
+                answer += CAFELATTE;
+            } else {
+                answer+=AMERICANO;
+            }
+        }
+
+        return answer;
+    }
+}
+/*
+class Solution {
+    private static final int AMERICANO = 4500;
+    private static final int CAFELATTE = 5000;
+    
     public int solution(String[] order) {
         int black=0, latte=0;
         
@@ -20,3 +39,4 @@ class Solution {
         return AMERICANO*black + CAFELATTE*latte;
     }
 }
+*/
