b20dcf0d100fca125e886088eadbf990ecb82bad
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-02-26 10:31:40 +0900

    [level 0] Title: qr code, Time: 0.36 ms, Memory: 70.7 MB -BaekjoonHub

--- Java/프로그래머스/0/181903. qr code/qr code.java
+++ Java/프로그래머스/0/181903. qr code/qr code.java
@@ -1,5 +1,19 @@
 class Solution {
     public String solution(int q, int r, String code) {
+        StringBuffer answer = new StringBuffer();
+        
+        for(int i=0; i<code.length(); i++){
+            if(i%q == r){
+                answer.append(code.charAt(i));
+            }
+        }
+        
+        return answer.toString();
+    }
+}
+/*
+class Solution {
+    public String solution(int q, int r, String code) {
         StringBuffer answer = new StringBuffer();        
         String[] c = code.split("");
         
@@ -12,3 +26,4 @@ class Solution {
         return answer.toString();
     }
 }
+*/
