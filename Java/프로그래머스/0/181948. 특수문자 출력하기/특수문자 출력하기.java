214a6c2226db00331f1fbb290b2c17601aaded20
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-02-27 16:31:08 +0900

    [level 0] Title: 특수문자 출력하기, Time: 129.95 ms, Memory: 61 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+/*
+", \ 등은 char로 표현하거나 \역슬래시 붙여서 이스케이프 문자로 표현"
+*/
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("!@#$%^&*(\\'\"<>?:;");
+    }
+}
