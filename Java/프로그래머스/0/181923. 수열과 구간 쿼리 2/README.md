b79422773344c083bfbbcb937fde018fa6c62ca5
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-03-02 17:05:06 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 4.64 ms, Memory: 77.6 MB -BaekjoonHub

--- Java/프로그래머스/0/181923. 수열과 구간 쿼리 2/README.md
+++ Java/프로그래머스/0/181923. 수열과 구간 쿼리 2/README.md
@@ -1,10 +1,10 @@
 # [level 0] 수열과 구간 쿼리 2 - 181923 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181923) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181923#) 
 
 ### 성능 요약
 
-메모리: 89.4 MB, 시간: 6.07 ms
+메모리: 77.6 MB, 시간: 4.64 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 02일 16:58:03
+2025년 03월 02일 17:05:04
 
 ### 문제 설명
 
--- Java/프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ Java/프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -3,6 +3,28 @@
 [1, 11, 2, 3] [[0, 3, 1]] [2]
 */
 import java.util.*;
+class Solution {    
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int[queries.length];
+        Arrays.fill(answer, -1);
+        
+        for(int q=0; q<queries.length; q++){            
+            for(int i=queries[q][0]; i<=queries[q][1]; i++){                
+                if(arr[i] > queries[q][2]){
+                    // arr에 0 포함 가능하므로 0으로 비교 불가. 다른 값으로 초기화
+                    // 기존에 저장된 값과 arr[i] 비교해서 작은 값 선택
+                    // 처음 비교하는 거면 arr[i] 저장해야 함.
+                    answer[q] = answer[q]==-1? arr[i] : Math.min(answer[q], arr[i]);
+                }
+            }
+            
+        }
+        
+        return answer;
+    }
+}
+/*
+import java.util.*;
 class Solution {
     private static final int arrMax = 1000000;
     
@@ -28,3 +50,4 @@ class Solution {
         return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
+*/
