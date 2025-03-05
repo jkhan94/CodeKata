cfbab4986b6e6346c0c0c3acb2330b429584c5bb
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-03-06 03:42:21 +0900

    [level 0] Title: 배열 만들기 6, Time: 36.58 ms, Memory: 139 MB -BaekjoonHub

--- Java/프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
+++ Java/프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -1,16 +1,24 @@
+
+/*
+Stack은 벡터를 상속 받아 스택 메소드만 추가됨.
+벡터는 멀티스레드 환경에서 thread safe하게 동작하도록 락을 걸어서 메소드 동기화.
+즉, synchronized 키워드 때문에 한 번에 한 스레드만 접근 가능.
+
+성능 떨어지니까 ArrayDeque 사용 권장.
+*/
 import java.util.*;
 class Solution {
     public int[] solution(int[] arr) {
-        Stack<Integer> stk = new Stack<>();
+        ArrayDeque<Integer> stk = new ArrayDeque<>();
         
         for(int i=0; i<arr.length; i++){
             if(stk.isEmpty()){
-                stk.push(arr[i]);
+                stk.addLast(arr[i]);
             } else {
-                if(stk.peek() == arr[i]){
-                    stk.pop();
+                if(stk.getLast() == arr[i]){
+                    stk.removeLast();
                 } else {
-                    stk.push(arr[i]);
+                    stk.addLast(arr[i]);
                 }
             }
         }
