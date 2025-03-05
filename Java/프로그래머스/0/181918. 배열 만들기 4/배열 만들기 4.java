5c1cf345db80dcbf9d519e590bd85cc6a4c43f65
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-03-05 18:55:07 +0900

    [level 0] Title: 배열 만들기 4, Time: 26.20 ms, Memory: 88.6 MB -BaekjoonHub

--- Java/프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
+++ Java/프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -4,15 +4,11 @@ class Solution {
         ArrayList<Integer> stk = new ArrayList<>();
         
         for(int i=0; i<arr.length; i++){
-            if(stk.isEmpty()){
+            if(stk.isEmpty() || stk.get(stk.size()-1) <arr[i]){
                 stk.add(arr[i]);
             } else {
-                if(stk.get(stk.size()-1) <arr[i]){
-                    stk.add(arr[i]);
-                } else {
-                    stk.remove(stk.size()-1);
-                    i--;
-                }
+                stk.remove(stk.size()-1);
+                i--;                
             }
         }
         
