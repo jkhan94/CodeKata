2c5dbb408926e49a5c7aedd2a79726876f0d6978
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-03-07 23:26:26 +0900

    [level 0] Title: 조건 문자열, Time: 0.03 ms, Memory: 73.8 MB -BaekjoonHub

--- Java/프로그래머스/0/181934. 조건 문자열/조건 문자열.java
+++ Java/프로그래머스/0/181934. 조건 문자열/조건 문자열.java
@@ -1,6 +1,36 @@
 class Solution {
     public int solution(String ineq, String eq, int n, int m) {
         int answer = 0;
+        
+        if(ineq.equals(">")){
+            if(eq.equals("=")){
+                if(n >= m){
+                    answer = 1;
+                }
+            } else {
+                if(n > m){
+                    answer = 1;
+                }
+            }
+        } else {
+            if(eq.equals("=")){
+                if(m >= n){
+                    answer = 1;
+                }
+            } else {
+                if(m > n){
+                    answer = 1;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
+/*
+class Solution {
+    public int solution(String ineq, String eq, int n, int m) {
+        int answer = 0;
         String cond = ineq+eq;
         
         switch(cond){
@@ -24,36 +54,6 @@ class Solution {
                     answer = 1;
                 }
                 break;                
-        }
-        
-        return answer;
-    }
-}
-/*
-class Solution {
-    public int solution(String ineq, String eq, int n, int m) {
-        int answer = 0;
-        
-        if(ineq.equals(">")){
-            if(eq.equals("=")){
-                if(n >= m){
-                    answer = 1;
-                }
-            } else {
-                if(n > m){
-                    answer = 1;
-                }
-            }
-        } else {
-            if(eq.equals("=")){
-                if(m >= n){
-                    answer = 1;
-                }
-            } else {
-                if(m > n){
-                    answer = 1;
-                }
-            }
         }
         
         return answer;
