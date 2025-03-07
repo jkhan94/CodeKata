68eac4736f4ad21a8b5ddb07ca6f3342ef0397f7
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-03-08 01:34:24 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 10.40 ms, Memory: 73.6 MB -BaekjoonHub

--- Java/프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
+++ Java/프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -2,14 +2,17 @@ class Solution {
     public String solution(String my_string, int[][] queries) {
         String[] answer = my_string.split("");
         int s=0, e=0;
+        String temp = "";
         
         for(int i=0; i<queries.length; i++){
             s=queries[i][0];
             e=queries[i][1];
-            for(int j=s, k=e; j<=e; j++){
-                answer[j] = my_string.charAt(k--)+"";
+            for(int j=s, k=e; j<(s+e+1)/2; j++, k--){                
+                temp = answer[j];
+                answer[j] = answer[k];
+                answer[k]=temp;
             }            
-            my_string=String.join("",answer);
+            // System.out.println(String.join("",answer));
         }
         
         return String.join("",answer);
