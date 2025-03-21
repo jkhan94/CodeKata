2b2b7a60657747a49b4b85e0288038d82931bfc1
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-03-21 19:12:47 +0900

    [level 1] Title: [1차] 비밀지도, Time: 2.88 ms, Memory: 80.4 MB -BaekjoonHub

--- Java/프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ Java/프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,45 +1,21 @@
 import java.lang.*;
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String[arr1.length];
-        String[] a1 = new String[n];
-        String[] a2 = new String[n];
+        String[] answer = new String[n];
         StringBuffer sb = new StringBuffer();
-        int len = 0;
+        sb.append("%").append(n).append("s");
+        String format = sb.toString();
+        
+        
         
         for(int i=0; i<n; i++){
-            sb.append(Integer.toBinaryString(arr1[i]));
-            if(sb.length() < n){
-                len = n-sb.length();
-                for(int j=0; j<len; j++){
-                    sb.insert(0,"0");
-                }                
-            }
-            a1[i] = sb.toString();            
-            sb.delete(0,sb.length());
-            
-            sb.append(Integer.toBinaryString(arr2[i]));
-            if(sb.length() < n){
-                len = n-sb.length();
-                for(int k=0; k<len; k++){                    
-                    sb.insert(0,"0");
-                }
-            }
-            a2[i] = sb.toString();            
-            sb.delete(0,sb.length());
+            answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
+            answer[i] = String.format(format,answer[i]);
+            answer[i] = answer[i].replaceAll("1","#");
+            answer[i] = answer[i].replaceAll("0"," ");
         }
                 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(a1[i].charAt(j)=='0' && a2[i].charAt(j)=='0'){
-                    sb.append(" ");
-                } else {
-                    sb.append("#");
-                }
-            }
-            answer[i] = sb.toString();
-            sb.delete(0, sb.length());
-        }
+
         
         return answer;
     }
