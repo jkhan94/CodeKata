759d3653eba4cd9241869e732e8d614f5b60e84d
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-03-29 00:03:48 +0900

    [level 1] Title: 소수 찾기, Time: 224.00 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,24 @@
+/*
+에라토스테네스의 체: n의 제곱근이하만큼 2부터 반복해서 나누어 떨어지지않으면 소수.
+*/
+class Solution {
+    public int solution(int n) {
+        int answer =  0;
+        boolean isOdd = true;
+        
+        for(int i=2; i<=n; i++){
+            for(int j=2; j<=Math.sqrt(i); j++){
+                if(i%j == 0){
+                    isOdd = false;
+                    break;
+                }          
+            }
+            if(isOdd){
+                answer++;
+            }
+            isOdd = true;
+        }
+        
+        return answer;
+    }
+}
