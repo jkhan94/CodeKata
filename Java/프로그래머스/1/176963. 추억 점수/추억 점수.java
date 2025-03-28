a317d7ec9a6d245819d47fa4e8212f5bafe4c3f4
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-03-28 22:34:39 +0900

    [level 1] Title: 추억 점수, Time: 18.73 ms, Memory: 105 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,27 @@
+/*
+1. 배열 활용
+photo를 순회하면서 이름이 저장된 name의 인덱스 찾음.
+해당 인덱스의 yearning을 더함.
+
+2. Map 활용
+name을 키로, yearning을 값으로 이름-그리움 맵 만듬
+photo를 순회하면서 containsKey를 활용해 name을 키 값으로 값을 더함. 키가 없으면 0 더함.
+*/
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        
+        for(int i=0; i<photo.length; i++){
+            for(int j=0; j<photo[i].length; j++){
+                for(int k=0; k<name.length; k++){
+                    if(photo[i][j].equals(name[k])){
+                        answer[i]+=yearning[k];
+                        break;
+                    }
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
