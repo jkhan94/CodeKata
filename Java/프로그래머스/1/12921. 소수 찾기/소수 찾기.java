93735eb5a7a3a922d1fe584adf310110587aa7d5
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-03-29 00:12:01 +0900

    [level 1] Title: 소수 찾기, Time: 208.19 ms, Memory: 54.5 MB -BaekjoonHub

--- Java/프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ Java/프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -1,5 +1,7 @@
 /*
 에라토스테네스의 체: n의 제곱근이하만큼 2부터 반복해서 나누어 떨어지지않으면 소수.
+
+ㅓ<=Math.sqrt(i) 보다 j*j<=i가 단순 곱셈이라 더 빠름.
 */
 class Solution {
     public int solution(int n) {
@@ -7,7 +9,7 @@ class Solution {
         boolean isOdd = true;
         
         for(int i=2; i<=n; i++){
-            for(int j=2; j<=Math.sqrt(i); j++){
+            for(int j=2; j*j<=i; j++){
                 if(i%j == 0){
                     isOdd = false;
                     break;
