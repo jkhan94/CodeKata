f922d5c701e272691e44b98df2e00a2e71ea343b
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-04-01 02:11:52 +0900

    [level 1] Title: 실패율, Time: 8.06 ms, Memory: 86.6 MB -BaekjoonHub

--- Java/프로그래머스/1/42889. 실패율/실패율.java
+++ Java/프로그래머스/1/42889. 실패율/실패율.java
@@ -13,6 +13,62 @@
 import java.util.*;
 class Solution {
     public int[] solution(int N, int[] stages) {
+        int[] answer = new int[N];
+        int[] uncleared = new int[N+2];
+        int[] reached = new int[N+2];
+        double failRatio = 0.0;
+        List<Stage> failList = new ArrayList<>();
+        
+        for(int s : stages){           
+            uncleared[s]++;            
+        }
+        reached[N+1] = uncleared[N+1]; // 올클
+        for(int i=N; i>=1; i--){
+            reached[i] = uncleared[i] + reached[i+1]; // 해당 스테이지 미클 + 클리어한 유저
+        }
+        
+        for(int i=1; i<=N; i++){
+            failRatio = reached[i]==0? 0.0 : (double)uncleared[i]/reached[i];
+            failList.add(new Stage(i, failRatio));
+        }
+        // for(int i=0; i<failList.size(); i++){
+        //     System.out.println(failList.get(i).stageNumber+" "+failList.get(i).failRatio);
+        // }
+        
+        // a,b 순서로 비교 대상이 주어짐
+        // 연산값이 양수면 a, b의 위치 바꿈
+        Collections.sort(failList, (a, b) -> {
+            // 뒤값이 크므로 앞으로 이동
+            if (Double.compare(b.failRatio, a.failRatio) == 0) {
+                // 앞이 크므로 뒤로 이동
+                return Integer.compare(a.stageNumber, b.stageNumber);
+            }
+            return Double.compare(b.failRatio, a.failRatio);
+        });
+        
+        
+        for (int i=0; i<N; i++) {
+            answer[i] = failList.get(i).stageNumber;
+        }       
+                
+        return answer;
+    }
+    
+    static class Stage {
+        int stageNumber;
+        double failRatio;
+        
+        Stage(int stageNumber, double failRatio) {
+            this.stageNumber = stageNumber;
+            this.failRatio = failRatio;
+        }        
+    }
+}
+
+/*
+import java.util.*;
+class Solution {
+    public int[] solution(int N, int[] stages) {
         int idx = 0;
         int[] answer = new int[N];
         double[] uncleared = new double[N+2];
@@ -55,3 +111,4 @@ class Solution {
         return answer;
     }
 }
+*/
